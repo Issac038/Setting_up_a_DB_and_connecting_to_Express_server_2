ed77a651c67edf37764f3fdeb34f8516e35cc0f3
Issac038 <issac.antony@kalvium.community>
2025-01-08 10:35:13 +0530

    set up db done

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose')
+require('dotenv').config()
+const userSchema = require('./schema.js')
 
 const app = express();
 const port = 3010;
 
-app.use(express.static('static'));
+const mongoUrl = process.env.MONGO_URL
+mongoose.connect(mongoUrl)
+  .then(()=>{
+    console.log('Connected to database')
+  })
+  .catch(()=>{
+    console.log('Error connecting to database')
+  })
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+app.use(express.json());
+
+app.post('/api/users', async (req, res) => {
+  try {
+    const userData = req.body;
+
+    const newUser = new User(userData);
+    await newUser.save();
+
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      res
+        .status(400)
+        .json({ message: 'Validation error', details: err.message });
+    } else {
+      res.status(500).json({ message: 'Server error', details: err.message });
+    }
+  }
 });
 
 app.listen(port, () => {
